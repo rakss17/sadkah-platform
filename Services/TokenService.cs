ee5087c92f1b8f0e6cda43c705b91736c04578fc
rakss17 <bohariambulo17@gmail.com>
2026-05-11 18:17:56 +0800

    decrease token exp to 15 mins

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -31,7 +31,7 @@ namespace Sadkah.Backend.Services
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
+                Expires = DateTime.UtcNow.AddMinutes(15),
                 SigningCredentials = creds,
                 Issuer = _config["JWT:Issuer"],
                 Audience = _config["JWT:Audience"]
