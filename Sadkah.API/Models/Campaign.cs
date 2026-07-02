974ceb543c59461fd560b76fe341bed33bbda541
rakss17 <bohariambulo17@gmail.com>
2026-07-02 19:49:05 +0800

    feat: add location to campaign db table

--- Sadkah.API/Models/Campaign.cs
+++ Sadkah.API/Models/Campaign.cs
@@ -24,6 +24,12 @@ namespace Sadkah.API.Models
         public Guid CategoryId { get; set; }
         public CampaignCategory Category { get; set; } = null!;
         public List<Donation> Donations { get; set; } = new List<Donation>();
+        public string AddressLine1 { get; set; } = string.Empty;
+        public string? AddressLine2 { get; set; }
+        public string Barangay { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Province { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? ArchivedAt { get; set; }
