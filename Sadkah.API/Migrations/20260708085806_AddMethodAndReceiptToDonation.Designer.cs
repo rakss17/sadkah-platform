939d99abdb7267c57987077a86f8d725fdc6e02a
rakss17 <bohariambulo17@gmail.com>
2026-07-08 17:40:43 +0800

    feat: add method and receipt to donation table

--- Sadkah.API/Migrations/20260708085806_AddMethodAndReceiptToDonation.Designer.cs
+++ Sadkah.API/Migrations/20260708085806_AddMethodAndReceiptToDonation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sadkah.API.Data;
 
@@ -11,9 +12,11 @@ using Sadkah.API.Data;
 namespace Sadkah.API.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260708085806_AddMethodAndReceiptToDonation")]
+    partial class AddMethodAndReceiptToDonation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
